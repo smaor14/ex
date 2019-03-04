d4714b354e2667ca2a5619ecbc04e75f110619c0
smaor14 <48226814+smaor14@users.noreply.github.com>
2019-03-05 00:53:00 +0200

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,22 @@
+import pika
+import json
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='message')
+path = input("Enter db path: ")
+formating = input("Enter the output file format: ")
+
+
+data = {
+    "path" : path,
+    "format": formating
+    }
+message = json.dumps(data)
+channel.basic_publish(exchange='',
+                      routing_key='message',
+                      body=message)
+print(" [x] Sent data")
+connection.close()
+
